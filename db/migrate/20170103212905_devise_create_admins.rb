d01c3fdb5469879fb0f83181278cafefa12c924c
qczhang18 <aznyqjai@gmail.com>
2017-01-03 16:32:07 -0500

    turn all devise admin options  ON

--- db/migrate/20170103212905_devise_create_admins.rb
+++ db/migrate/20170103212905_devise_create_admins.rb
@@ -2,6 +2,8 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.0]
   def change
     create_table :admins do |t|
       ## Database authenticatable
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -20,15 +22,15 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.0]
       t.inet     :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
 
 
       t.timestamps null: false
@@ -36,7 +38,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.0]
 
     add_index :admins, :email,                unique: true
     add_index :admins, :reset_password_token, unique: true
-    # add_index :admins, :confirmation_token,   unique: true
-    # add_index :admins, :unlock_token,         unique: true
+    add_index :admins, :confirmation_token,   unique: true
+    add_index :admins, :unlock_token,         unique: true
   end
 end
