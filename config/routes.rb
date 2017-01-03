c4be4cb6548b566e82e5c301cae31f0944741380
qczhang18 <aznyqjai@gmail.com>
2017-01-03 15:48:58 -0500

    corrected path from /user to /users => points correctly now

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users,
-    path: 'user',
+
+    path: 'users',
     path_names: { sign_in: 'login',
                   sign_out: 'logout',
                   password: 'secret',
                  # confirmation: 'verification',
                  # unlock: 'unblock',
                  # registration: 'register',
-                  sign_up: 'cmon_let_me_in' }
+                  sign_up: 'register' }
 
 
   #
